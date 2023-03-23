05a6769b0dbc9af77cee2b02a559a95c0e61a1bf
YeenYan <markiantalan@gmail.com>
2023-03-23 13:43:02 +0800

    modify some changes

--- src/main.js
+++ src/main.js
@@ -1,15 +1,11 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import Vue2Editor from "vue2-editor";
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
 
-Vue.use(Vue2Editor);
+import './assets/main.css'
 
-Vue.config.productionTip = false;
+const app = createApp(App)
 
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount("#app");
+app.use(router)
+
+app.mount('#app')
